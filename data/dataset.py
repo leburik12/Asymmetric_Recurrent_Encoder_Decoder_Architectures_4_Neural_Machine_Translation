715574e77b5b7cb90ab012883bf8ebb054b88e73
leburik12 <leburikplc@outlook.com>
2026-01-01 12:21:42 +0300

    fixing dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,4 +1,4 @@
-class TranslationDataset(Dataset):
+class TranslationDataset:
     def __init__(self, src_sentences, tgt_sentences, src_tokenizer, tgt_tokenizer):
         self.src, self.src_len = src_tokenizer.tokenize_and_pad(src_sentences)
         self.tgt, self.tgt_len = tgt_tokenizer.tokenize_and_pad(tgt_sentences)
